82590032ce6926366caa634a88c5526f513e2ec2
artem <arte@kb.ri>
2022-08-18 13:13:27 +0300

    done loginVC next step photoViewVC

--- IChat/SignUpViewController.swift
+++ IChat/SignUpViewController.swift
@@ -11,23 +11,30 @@ class SignUpViewController: UIViewController {
     
     let welcomeLabel = UILabel(text: "Good to see you!", font: .avenir26())
     
-    let emailLabel = UILabel(text: "Email", font: .avenir26())
-    let passwordLabel = UILabel(text: "Password", font: .avenir26())
-    let confirmPasswordLabel = UILabel(text: "Confirm password", font: .avenir26())
-    let alreadyOnboardLabel = UILabel(text: "Already onboard", font: .avenir26())
+    let emailLabel = UILabel(text: "Email")
+    let passwordLabel = UILabel(text: "Password")
+    let confirmPasswordLabel = UILabel(text: "Confirm password")
+    let alreadyOnboardLabel = UILabel(text: "Already onboard")
     
     let emailTextField = OneLineTextField(font: .avenir20())
     let passwordTextField = OneLineTextField(font: .avenir20())
     let confirmPasswordTextField = OneLineTextField(font: .avenir20())
     
     let signUpButton = UIButton(title: "Sign Up", titleColor: .white, backgroundColor: .buttonDark(), cornerRadius: 4)
-    let loginButton = UIButton()
+    let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Login", for: .normal)
+        button.setTitleColor(.buttonRed(), for: .normal)
+        button.titleLabel?.font = .avenir20()
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         loginButton.setTitle("Login", for: .normal)
         loginButton.setTitleColor(.buttonRed(), for: .normal)
-        view.backgroundColor = .orange
+        loginButton.titleLabel?.font = .avenir20()
+        view.backgroundColor = .white
         setupConstraints()
     }
 }
@@ -35,8 +42,56 @@ class SignUpViewController: UIViewController {
 //MARK: - Setup Contraints
 
 extension SignUpViewController {
+    
     private func setupConstraints() {
+        let emailStackView = UIStackView(arrangedSubviews: [emailLabel,emailTextField], axis: .vertical, spacing: 0)
+        let passwordStackView = UIStackView(arrangedSubviews: [passwordLabel,passwordTextField], axis: .vertical, spacing: 0)
+        let confirmStackView = UIStackView(arrangedSubviews: [confirmPasswordLabel,confirmPasswordTextField], axis: .vertical, spacing: 0)
         
+        signUpButton.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        
+        let stackView = UIStackView(arrangedSubviews: [
+            emailStackView,
+            passwordStackView,
+            confirmStackView,
+            signUpButton
+        ],
+                                    axis: .vertical,
+                                    spacing: 40)
+        
+        loginButton.contentHorizontalAlignment = .leading
+        let bottomStackView = UIStackView(arrangedSubviews: [
+            alreadyOnboardLabel,
+            loginButton
+        ],
+                                          axis: .horizontal,
+                                          spacing: 10)
+        bottomStackView.alignment = .firstBaseline
+        
+        welcomeLabel.translatesAutoresizingMaskIntoConstraints = false
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        bottomStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(welcomeLabel)
+        view.addSubview(stackView)
+        view.addSubview(bottomStackView)
+        
+        NSLayoutConstraint.activate([
+            welcomeLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 160),
+            welcomeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor, constant: 160),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
+        
+        NSLayoutConstraint.activate([
+            bottomStackView.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 60),
+            bottomStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            bottomStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
     }
 }
 
